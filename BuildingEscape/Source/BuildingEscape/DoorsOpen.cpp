e8372f9fdf65df9e27384d11fd00d37b3dd29746
Viet Hoang Pham <v3pro.hn97@gmail.com>
2016-09-06 15:50:27 -0400

    Moving Objects with c++

---
+++ BuildingEscape/Source/BuildingEscape/DoorsOpen.cpp
@@ -0,0 +1,42 @@
+// Copyright Viet Pham 2016.
+
+#include "BuildingEscape.h"
+#include "DoorsOpen.h"
+
+
+// Sets default values for this component's properties
+UDoorsOpen::UDoorsOpen()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDoorsOpen::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Find the owning Actor
+    AActor* Owner = GetOwner();
+    
+    //Create a rotator
+    FRotator NewRotation = FRotator(0.0f, -60.0f, 0.0f);
+    
+    //Set the door rotation
+    Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UDoorsOpen::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
---
+++ BuildingEscape/Source/BuildingEscape/DoorsOpen.h
@@ -0,0 +1,26 @@
+// Copyright Viet Pham 2016.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "DoorsOpen.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UDoorsOpen : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDoorsOpen();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+		
+	
+};
