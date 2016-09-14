7dbf6fee2ff60fbb21eb21bcff5718c1ae5a6885
Viet Hoang Pham <v3pro.hn97@gmail.com>
2016-09-13 22:43:43 -0400

    Input Binding

--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -27,4 +27,8 @@ private:
     
     UPhysicsHandleComponent *PhysicsHandle = nullptr;
 	
+    UInputComponent *InputComponent = nullptr;
+    
+    // Ray-cast and grab what's in reach
+    void Grab();
 };
